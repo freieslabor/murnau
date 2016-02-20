eed82c9504184e34da4ea1b4fb2623dc7b3c5d51
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2016-02-20 15:11:38 +0100

    lib: murnau: whitespace consistency cleanup
    Elixir uses Soft-Tabs for indentation. This is of course the wrong way
    of doing things, nevertheless change to the preferred style for consistency.
    
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- lib/murnau.ex
+++ lib/murnau.ex
@@ -1,18 +1,17 @@
 defmodule Murnau do
+  require Logger
   use Application
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
+    Logger.debug "#{__MODULE__}.start"
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Murnau.Worker, [arg1, arg2, arg3]),
+      worker(Task, [Murnau.Telegram, :accept, [0]]),
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Murnau.Supervisor]
     Supervisor.start_link(children, opts)
   end
