67dcbef188cf1985b95f9c30b43a9b82dae16c88
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2016-05-29 21:34:01 +0200

    config: prod: add missing ctrl_api
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- config/prod.exs
+++ config/prod.exs
@@ -4,6 +4,7 @@ config :logger, level: :debug
 
 config :murnau,
   ctrl_adapter: Murnau.Adapter.Telegram,
+  ctrl_api: Murnau.Adapter.Telegram.Api,
   ctrl_port: 80,
   telegram_url: "https://api.telegram.org",
   labor_adapter: Murnau.Adapter.Labor,
