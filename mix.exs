b7d5e02230b4ba04188440300b02ae26245d40e9
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2016-06-04 18:00:16 +0200

    mix.exs: add coveralls
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule Murnau.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test,
+                         "coveralls.detail": :test,
+                         "coveralls.post": :test,
+                         "coveralls.html": :test],
+     ]
   end
 
   # Configuration for the OTP application
@@ -35,6 +41,7 @@ defmodule Murnau.Mixfile do
      {:inch_ex, only: :docs},
      {:exrm, "~> 1.0.0" },
      {:poison, "~> 2.1.0"},
+     {:excoveralls, "~> 0.5", only: :test},
      {:cowboy, "~> 1.0.0", only: :test},
      {:plug, "~> 1.0", only: :test},]
   end
