f2f96f428db738a69012d97fd0ede3eba3bbead6
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2016-06-12 11:27:09 +0200

    test: httpclient: add test for content-type
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- test/server/httptest.ex
+++ test/server/httptest.ex
@@ -1,26 +1,34 @@
 defmodule HTTPTest do
-  @header [{"Server", "nginx/1.10.0"},
-           {"Date", "Sun, 05 Jun 2016 15:21:49 GMT"},
-           {"Content-Type", "application/json"},
-           {"Content-Length", "84"},
-           {"Connection", "keep-alive"},
-           {"Access-Control-Allow-Origin", "*"},
-           {"Access-Control-Allow-Methods", "GET, POST, OPTIONS"},
-           {"Access-Control-Expose-Headers",
-            "Content-Length,Content-Type,Date,Server,Connection"},
-           {"Strict-Transport-Security", "max-age=31536000; includeSubdomains"}]
   @url "https://api.testlegram.org/bot1234/"
   @params "?timeout=5&offset="
 
+  def get(@url <> "getUpdates" <> @params <> "0" <> _, [], _) do
+    {:ok, %HTTPoison.Response{body: "<html></html>",
+                              status_code: 200,
+                              headers: header("text/html")}}
+  end
   def get(@url <> "getUpdates" <> @params <> "1" <> _, [], _) do
     {:ok, %HTTPoison.Response{body: bot_command("1", "close"),
                               status_code: 200,
-                              headers: @header}}
+                              headers: header("application/json")}}
   end
   def get(@url <> "getUpdates" <> @params <> "2" <> _, [], _) do
     {:ok, %HTTPoison.Response{body: bot_command("2", "open"),
                               status_code: 200,
-                              headers: @header}}
+                              headers: header("application/json")}}
+  end
+
+  defp header(type) do
+    [{"Server", "nginx/1.10.0"},
+     {"Date", "Sun, 05 Jun 2016 15:21:49 GMT"},
+     {"Content-Type", type},
+     {"Content-Length", "84"},
+     {"Connection", "keep-alive"},
+     {"Access-Control-Allow-Origin", "*"},
+     {"Access-Control-Allow-Methods", "GET, POST, OPTIONS"},
+     {"Access-Control-Expose-Headers",
+      "Content-Length,Content-Type,Date,Server,Connection"},
+     {"Strict-Transport-Security", "max-age=31536000; includeSubdomains"}]
   end
 
   defp bot_command(id, cmd) do
--- test/server/telegram_api_test.exs
+++ test/server/telegram_api_test.exs
@@ -7,4 +7,8 @@ defmodule Murnau.Server.Telegram.ApiTest do
     assert {:ok, %{message: %{text: "/close"}, update_id: 1}} = Murnau.Adapter.Telegram.Api.getupdate(1)
     assert {:ok, %{message: %{text: "/open"}, update_id: 2}} = Murnau.Adapter.Telegram.Api.getupdate(2)
   end
+
+  test "getUpdate handles non-JSON" do
+    assert {:error, []} = Murnau.Adapter.Telegram.Api.getupdate(0)
+  end
 end
