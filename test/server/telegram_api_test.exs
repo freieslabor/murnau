823504efbf05e88b231e6a9005f52cefe9db11a3
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2016-06-19 01:09:51 +0200

    test: murnau: telegram: use alias to add readability
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- test/server/telegram_api_test.exs
+++ test/server/telegram_api_test.exs
@@ -1,25 +1,27 @@
 defmodule Murnau.Server.Telegram.ApiTest do
+  alias Murnau.Adapter.Telegram.Api
+  alias Murnau.Adapter.Telegram
   use ExUnit.Case, async: true
   doctest Murnau
   require Logger
 
   test "getUpdate returns correct command" do
-    assert {:ok, %{message: %{text: "/close"}, update_id: 1}} = Murnau.Adapter.Telegram.Api.getupdate(1)
-    assert {:ok, %{message: %{text: "/open"}, update_id: 2}} = Murnau.Adapter.Telegram.Api.getupdate(2)
+    assert {:ok, %{message: %{text: "/close"}, update_id: 1}} = Api.getupdate(1)
+    assert {:ok, %{message: %{text: "/open"}, update_id: 2}} = Api.getupdate(2)
   end
 
   test "getUpdate handles non-JSON" do
-    assert {:error, []} = Murnau.Adapter.Telegram.Api.getupdate(0)
+    assert {:error, []} = Api.getupdate(0)
   end
 
   test "getUpdate handles 403" do
-    assert {:forbidden, []} = Murnau.Adapter.Telegram.Api.getupdate(403)
+    assert {:forbidden, []} = Api.getupdate(403)
   end
   test "getUpdate handles 409" do
-    assert {:conflict, []} = Murnau.Adapter.Telegram.Api.getupdate(409)
+    assert {:conflict, []} = Api.getupdate(409)
   end
   test "sendMessage returns correct response" do
-    chat = %Murnau.Adapter.Telegram.Chat{id: 9}
-    assert {:ok, %{text: "foobar"}} = Murnau.Adapter.Telegram.Api.send_message(chat ,"foobar")
+    chat = %Telegram.Chat{id: 9}
+    assert {:ok, %{text: "foobar"}} = Api.send_message(chat ,"foobar")
   end
 end
