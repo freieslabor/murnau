e7f1d469c036bba4093fa14639d737cf98fdd522
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2016-11-05 20:09:33 +0100

    config: update dev setup
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- config/dev.exs
+++ config/dev.exs
@@ -8,11 +8,8 @@ config :murnau,
   ctrl_adapter: Murnau.Adapter.Telegram,
   ctrl_api: Murnau.Adapter.Telegram.Api,
   ctrl_port: 80,
-  telegram_url: "https://api.testlegram.org",
+  telegram_url: "https://api.telegram.org",
   labor_adapter: Murnau.Adapter.Labor,
-  labor_url: "https://freieslabor.org",
-  labor_chat_id: -2,
-  telegram_token: "1234",
-  labor_user: "murnau",
-  labor_token: "O",
-  open_timeout: 1 * 60 * 1000
+  labor_url: "https://freieslabor.org"
+
+import_config "prod.secret.exs"
